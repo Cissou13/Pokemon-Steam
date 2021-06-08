8e703768b01c58ec90450ca2caae51a0471051fb
Cyrille F <cyrille.fabre@gmail.com>
2021-06-09 01:59:12 +0200

    Only Team left

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,12 +3,26 @@ import './Homepage.css';
 
 export default function Homepage() {
   return (
-    <div>
-      <main className='home-container'>
-        <section className='homepage-container'>Partie 1</section>
-        <section className='homepage-container'>Partie 2</section>
-        <section className='homepage-container'>Partie 3</section>
-      </main>
-    </div>
+    <main className='homepage-container'>
+      <h1>Bienvenue sur le site officiel du fangame Pokemon Steam</h1>
+      <section className='homepage-content'>
+        <p>
+          Ce site fait la jonction entre les différents acteurs et ressources
+          qui vont t'aider.
+        </p>
+        <p>
+          Aussi vous trouverez quelques petites infos, des liens utiles, les
+          questions les plus fréquemments posées,et l'avancée concernant mes
+          projets.
+        </p>
+      </section>
+      <section className='homepage-content'>
+        <p>
+          N'hésitez pas à rejoindre le discord officiel du jeu, ainsi que le
+          wiki où 90% de vos questions trouveront réponse.{' '}
+        </p>
+        <p> Jetez un oeil à la catégorie "Liens Utiles" en bas du site.</p>
+      </section>
+    </main>
   );
 }
